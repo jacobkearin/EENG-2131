6fc52fa910f50bfcd54f5298998dea98c4f92b29
Jacob Kearin <71102319+jacobkearin@users.noreply.github.com>
2021-11-17 17:20:33 -0600

    Update HW3_2.v

--- HW3/Question 2/HW3_2.v
+++ HW3/Question 2/HW3_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/16/2021 05:06:08 PM
-// Design Name: 
-// Module Name: HW3_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module HW3_2(A, B, C, F1);
 input A, B, C;
