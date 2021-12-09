4c6790f541ea14d472f2642a7948bf6254ca7da2
Jacob Kearin <71102319+jacobkearin@users.noreply.github.com>
2021-12-08 19:11:00 -0600

    Update motor_project.v

--- final_project/sources_1/motor_project.v
+++ final_project/sources_1/motor_project.v
@@ -6,18 +6,16 @@
 // Create Date: 11/23/2021 05:04:48 PM
 // Design Name: BLDC driver
 // Module Name: 
-// Project Name: 
-// Target Devices: 
+// Project Name: BLDC_driver
+// Target Devices: Basys-3
 // Tool Versions: 
 // Description: 
-// variable frquency driver for 3 phase BLDC motor with included hall sensors
-// brushless gearmotor that this was designed for has 0-98hz variability
-// for use with BLDC with different frequency limitations, the primary module "main_driver" will need modifications to max counters and multipliers. 
-//
-// Revision 0.01 - File Created
-// Additional Comments:
+//      Program was designed for Basys-3 development board, IO may need to be modified for use with other products
+//      Program was designed around BODINE BLDC 12V gearmotor with built-in hall sensors and 43.9:1 reduction ratio
+//      For use with other BLDC, modifications to program may be necessary. See annotations below for guidance
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
 
 //module for creating motor driver signals
 //see motor datasheet
@@ -44,10 +42,10 @@ always @(posedge clk) begin
     else case (state)
         s0: begin
             if (hall == 3'b001) state <= s1;
-            else if (hall == 3'b000) state <= s2;
-            else if (hall == 3'b100) state <= s3;
-            else if (hall == 3'b110) state <= s4;
-            else if (hall == 3'b111) state <= s5;
+            else if (hall == 3'b000) state <= s2;   // for sensorless BLDC either change
+            else if (hall == 3'b100) state <= s3;   // this section to start at a specific 
+            else if (hall == 3'b110) state <= s4;   // state OR set top module hall value
+            else if (hall == 3'b111) state <= s5;   // to 1'b000, NOT BOTH
             else if (hall == 3'b011) state <= s6;
             else state <= s0;
         end
@@ -64,10 +62,10 @@ endmodule
 
 //module for taking adc input from potentiometer for speed control
 //outputs a 16 bit value with most significant 12 bits being and output
-module adc_counter(CLK, jxADC, data_out);
+module adc_controller(CLK, jxADC, data_out);
 input CLK;
 input [6:0] jxADC;
-output reg [15:0] data_out;
+output reg [15:0] data_out; // 12 bit ADC data within most significant of 16 bit output
 
 wire enable;
 wire ready;
@@ -86,13 +84,13 @@ xadc_wiz_2  XLXI_7 (
         .drdy_out(ready));
 
 always @(posedge CLK) begin
-    data_out <= data; //((data[15:4] * 98) / 4096); //data;
+    data_out <= data; 
 end
 endmodule
 
 
-//motor driver module
-//see schematic attached for circuit layout
+// main motor driver module
+// see schematic attached for circuit layout
 module main_driver (clk, JXADC, sw, JA, JB);
 input clk;
 input [7:0] JXADC;
@@ -102,25 +100,22 @@ input [7:0] JA;     // basys-3 JA0, JA1, JA2 for 3 bit hall sensors
 output [5:0] JB;    // basys-3 JB0-6 for output signals to 3 phase transistor array
                     // use of pull-up NMOS required for ABC positive inputs
 
-reg [31:0] counter = 0;
-reg freq_clk = 0;
-wire [15:0] data;
-
+reg [31:0] freq_counter = 0;
+reg var_clk = 0;
+wire [15:0] data;   
 parameter min_count = 510200;   // half of minimum frequency counter
                                 // 100Mhz clk / (2 * max_motor_freq)
-
-adc_counter countdut(clk, JXADC, data);
+adc_controller dut0(clk, JXADC, data);
 
 always @(posedge clk) begin
-    counter <= counter +1;
-    if (counter >= (min_count + ((4096 - data[15:4]) ** 2))) begin 
-        counter <= 0;
-        freq_clk <= !freq_clk;
+    freq_counter <= freq_counter +1;
+    if (freq_counter >= (min_count + ((4096 - data[15:4]) ** 2))) begin 
+        freq_counter <= 0;
+        var_clk <= !var_clk;
     end
 end
 
-motor_driver dut(freq_clk, sw[0], sw[1], JB[0], JB[1], JB[2], JB[3], JB[4], JB[5], JA[2:0]);
+motor_driver dut1(var_clk, sw[0], sw[1], JB[0], JB[1], JB[2], JB[3], JB[4], JB[5], JA[2:0]);
 
 endmodule
 
-//addition of PMOD display WIP
